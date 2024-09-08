8b9ff158836d75967e4b64050afecf1fb7c3c96e
Anastasia Ivanova <anastasiaroubo@gmail.com>
2024-09-08 13:30:12 +0300

    feat: added the tasks 1-4

---
+++ src/TaskOne.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class TaskOne {
+
+    public static void main(String[] args) {
+        // Задача 1. Создать трехмерный массив из целых чисел, заполнить случайными числами от -100 до 100.
+        // С помощью циклов "пройти" по всему массиву и увеличить каждый элемент на заданное число.
+        // Пусть число, на которое будет увеличиваться каждый элемент,задается из консоли.
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[][][] threeDimensionalArray = new int[3][3][5];
+        Random random = new Random();
+
+        for (int i = 0; i < threeDimensionalArray.length; i++) {
+            for (int j = 0; j < threeDimensionalArray[i].length; j++) {
+                for (int k = 0; k < threeDimensionalArray[i][j].length; k++) {
+                    threeDimensionalArray[i][j][k] = random.nextInt(-100, 100);
+                }
+            }
+        }
+
+        System.out.println("Generated array: ");
+
+        for (int i = 0; i < threeDimensionalArray.length; i++) {
+            System.out.println(" ");
+            for (int j = 0; j < threeDimensionalArray[i].length; j++) {
+                System.out.println(Arrays.toString(threeDimensionalArray[i][j]) + " ");
+            }
+            System.out.println(" ");
+        }
+
+        System.out.println("Enter a number by which the every element will be increased: ");
+        int increasingNumber = scanner.nextInt();
+
+        for (int i = 0; i < threeDimensionalArray.length; i++) {
+            for (int j = 0; j < threeDimensionalArray[i].length; j++) {
+                for (int k = 0; k < threeDimensionalArray[i][j].length; k++) {
+                    threeDimensionalArray[i][j][k] += increasingNumber;
+                }
+            }
+        }
+
+        System.out.println("Changed array: ");
+        for (int i = 0; i < threeDimensionalArray.length; i++) {
+            System.out.println(" ");
+            for (int j = 0; j < threeDimensionalArray[i].length; j++) {
+                System.out.println(Arrays.toString(threeDimensionalArray[i][j]) + " ");
+            }
+            System.out.println(" ");
+        }
+
+        scanner.close();
+    }
+}
+
---
+++ src/TaskTwo.java
@@ -0,0 +1,37 @@
+public class TaskTwo {
+    public static void main(String[] args) {
+        // Задача 2. Шахматная доска
+        // Создать программу для раскраски шахматной доски с помощью цикла. Создать двумерный массив char'ов 8х8. С помощью циклов задать элементам массива значения B(Black) или W(White). Результат работы программы:
+        //W B W B W B W B
+        //B W B W B W B W
+        //W B W B W B W B
+        //B W B W B W B W
+        //W B W B W B W B
+        //B W B W B W B W
+        //W B W B W B W B
+        //B W B W B W B W
+
+        char[][] chessboard = new char[8][8];
+
+        for (int i = 0; i < chessboard.length; i++) {
+            for (int j = 0; j < chessboard[i].length; j++) {
+                char black = 'B';
+                char white = 'W';
+                if ((i + j) % 2 == 0) {
+                    chessboard[i][j] = white;
+                } else {
+                    chessboard[i][j] = black;
+                }
+            }
+        }
+
+        for (int i = 0; i < chessboard.length; i++) {
+            System.out.println();
+            for (int j = 0; j < chessboard[i].length; j++) {
+                System.out.print(chessboard[i][j] + " ");
+            }
+        }
+    }
+}
+
+
